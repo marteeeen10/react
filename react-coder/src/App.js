e8dac52a0b68b06aeb848f0bc1e9755cde81edfd
Martin <marteeeen@gmail.com>
2023-01-23 03:59:44 -0300

    navBar

--- react-coder/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-  <div>Hola</div>
-  );
-}
-
-export default App;
---
+++ react-coder/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+
+const CartWidget = () => {
+    return (
+        <div>
+            <button className="btn btn-danger">Carro</button>
+        </div>
+    );
+}
+
+export default CartWidget;
