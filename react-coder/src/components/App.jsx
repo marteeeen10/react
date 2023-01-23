02ee5fecc6b23ff2fd490cf1d6e93a99e8cba925
Martin <marteeeen@gmail.com>
2023-01-23 05:01:22 -0300

    Primera

--- react-coder/src/components/App.jsx
+++ react-coder/src/components/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./Navbar/Navbar";
-
+import ItemListContainer from "./ItemListContainer/ItemListContainer";
 const App = () => {
   return (
     <>
       <Navbar/>
+      <ItemListContainer saludo={'Hey Dude!'}/>
     </>
   );
 }
---
+++ react-coder/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+
+const ItemListContainer = ({saludo}) => {
+    return (
+        <>
+            <p>{saludo}</p>
+        </>
+    );
+}
+
+export default ItemListContainer;
